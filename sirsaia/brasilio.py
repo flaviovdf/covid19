79496b3d09be15acf3ba90b39f2b70c6e96dfa63
Flavio Figueiredo <flaviovdf@dcc.ufmg.br>
2020-04-25 21:33:43 -0300

    revamped plots

--- sirsaia/brasilio.py
+++ sirsaia/brasilio.py
@@ -37,3 +37,65 @@ def load_data(fpath):
     rv[rv < 0] = 0
 
     return rv
+
+
+def get_state(full_df, state):
+    work_df = full_df[['state', 'city', 'place_type',
+                       'confirmed', 'deaths']].copy()
+    work_df.index = pd.to_datetime(full_df['date'])
+    work_df = work_df.sort_index()
+
+    idx = (work_df['state'] == state) & (work_df['place_type'] == 'state')
+    state_df = work_df[idx]
+
+    first_day = np.nonzero(state_df.values)[0][0]
+    state_df = state_df[first_day:]
+
+    first = state_df['confirmed'].iloc[0]
+    state_df['confirmed'] = state_df['confirmed'].diff().fillna(0).copy()
+    state_df['confirmed'].values[state_df['confirmed'] < 0] = 0
+    state_df['confirmed'].values[0] = first
+
+    first = state_df['deaths'].iloc[0]
+    state_df['deaths'] = state_df['deaths'].diff().fillna(0).copy()
+    state_df['deaths'].values[state_df['deaths'] < 0] = 0
+    state_df['deaths'].values[0] = first
+
+    idx = (work_df['state'] == state) & \
+        (work_df['city'] == 'Importados/Indefinidos')
+    imported_df = work_df[idx]['confirmed']
+
+    state_df['imported'] = imported_df
+    state_df = state_df.rename(columns={'confirmed': 'local'}).fillna(0).copy()
+    last_day = np.nonzero(state_df['local'].values)[0][-1]
+
+    return state_df.iloc[:last_day+1].copy()
+
+
+def get_city(full_df, state, city):
+    work_df = full_df[['state', 'city', 'place_type',
+                       'confirmed', 'deaths']].copy()
+
+    work_df.index = pd.to_datetime(full_df['date'])
+    work_df = work_df.sort_index()
+
+    idx = (work_df['state'] == state) & (work_df['city'] == city)
+
+    city_df = work_df[idx]
+    first_day = np.nonzero(city_df.values)[0][0]
+    city_df = city_df[first_day:]
+
+    first = city_df['confirmed'].iloc[0]
+    city_df['confirmed'] = city_df['confirmed'].diff().fillna(0).copy()
+    city_df['confirmed'].values[city_df['confirmed'] < 0] = 0
+    city_df['confirmed'].values[0] = first
+
+    first = city_df['deaths'].iloc[0]
+    city_df['deaths'] = city_df['deaths'].diff().fillna(0).copy()
+    city_df['deaths'].values[city_df['deaths'] < 0] = 0
+    city_df['deaths'].values[0] = first
+
+    city_df = city_df.rename(columns={'confirmed': 'local'}).fillna(0).copy()
+    last_day = np.nonzero(city_df['local'].values)[0][-1]
+
+    return city_df.iloc[:last_day+1].copy()
--- sirsaia/plotutils.py
+++ sirsaia/plotutils.py
@@ -5,25 +5,26 @@ from scipy.interpolate import interp1d
 
 
 import numpy as np
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import pytz
 
 
-plt.rcParams['axes.labelsize'] = 20
-plt.rcParams['axes.titlesize'] = 20
-plt.rcParams['legend.fontsize'] = 20
-plt.rcParams['xtick.labelsize'] = 20
-plt.rcParams['ytick.labelsize'] = 20
-plt.rcParams['lines.linewidth'] = 2
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['text.usetex'] = True
-
-plt.style.use('tableau-colorblind10')
-plt.rcParams['figure.figsize'] = (12, 8)
+def init_matplotlib():
+    plt.rcParams['axes.labelsize'] = 24
+    plt.rcParams['axes.titlesize'] = 24
+    plt.rcParams['legend.fontsize'] = 24
+    plt.rcParams['xtick.labelsize'] = 24
+    plt.rcParams['ytick.labelsize'] = 24
+    plt.rcParams['lines.linewidth'] = 3
+    plt.rcParams['font.family'] = 'serif'
+    # plt.rcParams['text.usetex'] = True
 
 
 def despine(ax=None):
     if ax is None:
         ax = plt.gca()
+
     # Hide the right and top spines
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
@@ -35,7 +36,6 @@ def despine(ax=None):
 
 def plot_result(result_df, original_df=None):
     def flip_date(date_txt):
-        print(date_txt)
         y, m = date_txt[:10].split('-')[1:3]
         return '/'.join([m, y])
 
@@ -51,7 +51,6 @@ def plot_result(result_df, original_df=None):
 
     mean = result_df['Mean(R)']
     pm = (result_df['Quantile.0.975(R)'] - result_df['Quantile.0.025(R)']) / 2
-    # x = np.arange(len(mean))
 
     x = original_df.index[(result_df['t_end'] - 1).astype('i').values]
     plt.plot(x, mean, c='k', zorder=1, alpha=.8)
@@ -60,16 +59,14 @@ def plot_result(result_df, original_df=None):
                                                         flip_date(str(x[-1]))),
                  (x[-1], mean.values[-1]),
                  xytext=(x[-7], mean.values[-1] + 2),
-                 fontsize=14, horizontalalignment='center',
+                 fontsize=24, horizontalalignment='center',
                  arrowprops=dict(arrowstyle='fancy',
                                  color='0.5',
                                  shrinkB=5))
-    plt.scatter(x, mean, s=80, lw=.5, c=cmap(color_mapped(mean)),
+    plt.scatter(x, mean, s=160, lw=1, c=cmap(color_mapped(mean)),
                 edgecolors='k', zorder=2)
     ax = plt.gca()
     ax.xaxis_date()
-
-    # plt.plot(x, mean, label='R(t) +- .95CI')
 
     y_inf = result_df['Quantile.0.025(R)']
     y_sup = result_df['Quantile.0.975(R)']
@@ -80,24 +77,83 @@ def plot_result(result_df, original_df=None):
     plt.fill_between(x, lowfn(xi), highfn(xi), color='k', alpha=0.1, lw=0,
                      zorder=3)
 
-    import pytz
     brt = pytz.timezone('America/Sao_Paulo')
 
-    import matplotlib.dates as mdates
     ax.xaxis.set_major_locator(mdates.WeekdayLocator(tz=brt))
     ax.xaxis.set_minor_locator(mdates.DayLocator(tz=brt))
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m', tz=brt))
     plt.xticks(rotation=75)
 
-    # import datetime
-    # datenow = datetime.datetime.now()
-    # dstart = datetime.datetime(2020, 3, 1)
-    # plt.xlim(dstart, datenow)
-
     plt.ylim((0, y_sup.max() + 0.01))
     plt.yticks(np.arange(np.ceil(y_sup.max())))
     ax.axhline(1, linestyle='--', color='grey', zorder=4)
-    # plt.legend()
+
+    plt.title('Infectividade Estimada')
     plt.ylabel(r'$R(t) \pm .95$ CI')
     plt.xlabel(r'Data - $t$')
     despine()
+
+
+def plot_it(df, deaths=False):
+    if deaths:
+        data = df.deaths
+    else:
+        data = df.local
+
+    plt.plot(df.index, data, c='k', zorder=1, alpha=.8)
+    plt.scatter(df.index, data, s=160, lw=1, edgecolors='k', zorder=2)
+
+    brt = pytz.timezone('America/Sao_Paulo')
+
+    ax = plt.gca()
+    ax.xaxis_date(tz=brt)
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator(tz=brt))
+    ax.xaxis.set_minor_locator(mdates.DayLocator(tz=brt))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m', tz=brt))
+
+    plt.xticks(rotation=75)
+    if deaths:
+        plt.title('Mortes Confirmadas M(t) por dia')
+        plt.ylabel('M(t)')
+    else:
+        plt.title('Casos Confirmados I(t) por dia')
+        plt.ylabel('I(t)')
+
+    plt.xlabel('Data - t')
+    # plt.ylim((0, data.values.max() + 10))
+
+
+def plot_weekdiff(df):
+
+    df = df[['local']].resample('W-Sat').sum().copy()
+    first = df.iloc[0]
+
+    df = df.diff().copy()
+    df.iloc[0] = first
+    df = df.iloc[:-1]
+
+    positive = df > 0
+    plt.plot(df.index, df.local, c='k', zorder=1, alpha=.8)
+
+    if (~positive).sum().any():
+        plt.scatter(df[~positive].index, df[~positive].local, s=160, lw=1,
+                    edgecolors='k', zorder=2, color='g')
+
+    plt.scatter(df[positive].index, df[positive].local, color='r',
+                s=160, lw=1, edgecolor='k', zorder=2)
+
+    brt = pytz.timezone('America/Sao_Paulo')
+
+    ax = plt.gca()
+    ax.xaxis_date(tz=brt)
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator(tz=brt,
+                                                     byweekday=mdates.SA))
+    ax.xaxis.set_minor_locator(mdates.DayLocator(tz=brt))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m', tz=brt))
+    ax.axhline(0, linestyle='--', color='grey', zorder=4)
+
+    # plt.ylim((0, df.values.max() + 10))
+    plt.xticks(rotation=75)
+    plt.title('Diferença Entre Soma de Casos da Semana')
+    plt.ylabel('I(w) - I(w-1)')
+    plt.xlabel('Semana - w')
